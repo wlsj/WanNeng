9b323d36de70433bea2bc1b6428528ac99cbfe75
wlsj <wlsgithub@126.com>
2018-08-08 01:22:02 +0800

    net
    net

---
+++ wnlibrary/src/androidTest/java/net/BaseResponse.java
@@ -0,0 +1,41 @@
+package net;
+
+
+/**
+ * 网络返回基类 支持泛型
+ */
+public class BaseResponse<T> {
+
+    private int code;
+    private String msg;
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isOk() {
+        return code == 0;
+    }
+
+}
---
+++ wnlibrary/src/androidTest/java/net/CallBack.java
@@ -0,0 +1,14 @@
+package net;
+
+
+public abstract class CallBack {
+    public void onStart(){}
+
+    public void onCompleted(){}
+
+    abstract public void onError(Throwable e);
+
+    public void onProgress(long fileSizeDownloaded){}
+
+    abstract public void onSucess(String path, String name, long fileSize);
+}
