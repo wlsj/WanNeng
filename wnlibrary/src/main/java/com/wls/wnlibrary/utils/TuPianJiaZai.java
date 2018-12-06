beef62c107fa9f344ae2f4204882b5cad774f5b2
WLS <wlsgithub@126.com>
2018-12-06 09:21:17 +0800

    1
    1

---
+++ wnlibrary/src/main/java/com/wls/wnlibrary/utils/TuPianJiaZai.java
@@ -0,0 +1,34 @@
+package com.wls.wnlibrary.utils;
+
+/*
+ *
+ * wlsj 2018/12/06
+ *
+ * */
+
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+public class TuPianJiaZai {
+
+    public static void JiaZaiTuPian(Context mContext, String path, int zhanWei, int shiBai, boolean shiFouHuanCun,ImageView mImageView) {
+
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(zhanWei)
+                .error(shiBai)
+                .skipMemoryCache(shiFouHuanCun)
+                ;
+
+        Glide.with(mContext)
+                .load(path)
+                .apply(requestOptions)
+                .into(mImageView);
+
+    }
+
+
+}
